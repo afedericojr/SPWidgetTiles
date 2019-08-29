3eeb854cf5e01709910039e28aad0ac223ff9212
afedericojr <afedericojr@gmail.com>
2019-08-29 13:47:06 -0400

    Add files via upload
    added usage description

--- WidgetTiles.js
+++ WidgetTiles.js
@@ -1,13 +1,15 @@
 /**
  * @description Queries a SharePoint list and displays the query result in a widget tile.
- * @description Currently accepts $select queries and /ItemCount
+ * @description Currently accepts $select queries and /ItemCount.
+ * @description Use a data attribute of data-count as a boolean to get a filtered count of items.
  * @author AFEDERICO
  * @todo Test against lookup fields other than a person lookup
  * @requires DIV areas be defined with data attributes. Multiple divs with unique ID's may be used.
  * <div id="widget1" 
  * class="widget style4" 
  * data-heading="My Heading" 
- * data-labels='{"Title":"From"}' 
+ * data-labels='{"Title":"From"}'
+ * data-count=false
  * data-query="../_api/web/lists/getbytitle('List Name')/Items?$filter=Status eq 'Some Status'&$select=id,Title,To/Title,Field_x0020_Name, Qty, Status&$OrderBy=Id desc&$top=1&$expand=To">
  * </div>
  *
